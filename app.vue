bca88f24a99f22b0afe3958117d5d77d3acda082
akshit <akshit@smartshore.nl>
2023-11-05 12:27:12 +0530

    initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLoadingIndicator color="#800000" :throttle="0" :height="5" />
+    <NuxtPage />
+  </div>
+</template>
+<style>
+/* .page-enter-active,
+.page-leave-active {
+  transition: all 0.15s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+} */
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: foo
+
+    <NuxtLink :to="'/contact'">
+      CONTACT
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
